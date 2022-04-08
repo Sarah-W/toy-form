d8ecf3021432fb85f6617632a804ec9b3f2eb31f
Sarah-W <sarah.wheaton@gmail.com>
2022-04-08 12:05:28 +1200

    a start on the form

--- src/App.js
+++ src/App.js
@@ -6,24 +6,49 @@ function BlueDiv(){
 }
 
 
-function AnimalForm(){
 
-  return <></>
-}
 
-function ListMe(items){
+function ListMe({items}){
   const list = items.map(item => <li key={item}>{item}</li>)
   return <ul> {list} </ul> 
 }
 
-const items= ["red","green","blue"]
+function Select({labelid,labeltext,items}){
+  console.log(items)
+  const label = <label for={labelid}>{labeltext}</label>
+  const options = items.map(item =>{ 
+    return <option key={item} value={item}>
+      {item} 
+    </option>} 
+  )
+  return <>
+    {label}
+    <select id={labelid}>
+      {options}
+    </select>
+  </>
+}
+
+function AnimalForm(){
+
+  return <>
+    <Select items={animals} labelid={"animal"} labeltext={"Select the type of your animal: "}></Select>
+    <Select items={colours} labelid={"colour"} labeltext={"Select the colour of your animal: "}></Select>
+  </>
+}
+
+const colours= ["Blue", "Green", "Red", "Black","Brown"]
+const animals=[ "Bear","Tiger","Snake","Donkey"] 
+
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
-        <BlueDiv></BlueDiv>
-        <ListMe></ListMe>
+        {/* <BlueDiv></BlueDiv>
+        <ListMe items={myitems}></ListMe>
+        <ListMe items={animals}></ListMe> */}
+        <AnimalForm></AnimalForm>
       </div> 
     </div>
   );
